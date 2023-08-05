bde537c296965103c0118e9f602fca0ddd952298
Harsh Singh <95915982+harsh-s15@users.noreply.github.com>
2023-08-05 16:28:28 +0530

    Created config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+        apiKey: "AIzaSyDFaHx8iET00PaHcdSO6tFldTPwD7fvu5g",
+        authDomain: "laundry-iitj-ce4c9.firebaseapp.com",
+        databaseURL: "https://laundry-iitj-ce4c9-default-rtdb.firebaseio.com",
+        projectId: "laundry-iitj-ce4c9",
+        storageBucket: "laundry-iitj-ce4c9.appspot.com",
+        messagingSenderId: "929330828121",
+        appId: "1:929330828121:web:b571474df940eb02f57248"
+      };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase
